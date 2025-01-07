884d7b17ab0702d13aca30386490a03f07c30674
Letícia Vieira <lvieira268@gmail.com>
2025-01-06 21:24:29 -0300

    feat: add thumbnails assets
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ src/assets/thumbnails/index.ts
@@ -0,0 +1,9 @@
+import imagem01 from "./01.webp";
+import imagem02 from "./02.webp";
+import imagem03 from "./03.webp";
+import imagem04 from "./04.webp";
+import imagem05 from "./05.webp";
+import imagem06 from "./06.webp";
+import imagem07 from "./07.webp";
+
+export { imagem01, imagem02, imagem03, imagem04, imagem05, imagem06, imagem07 };
