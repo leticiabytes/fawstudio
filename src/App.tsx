a4d5fb457d6d3bf9de6757972cfe7c6c01396677
Letícia Vieira <lvieira268@gmail.com>
2025-01-06 21:29:30 -0300

    feat: updates home page
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { Header } from "./components/header";
+import { ShootingStars, StarsBackground } from "./components/ui";
+
+import { Hero, About, Showroom, CTA } from "./sections";
+import { Footer } from "./sections/footer";
+
 export function App() {
-  return <div></div>;
+  return (
+    <main className="bg-darker dark">
+      <Header />
+
+      {/* Content */}
+      <div className="sm:px-16 px-6 min-h-screen mt-12 pb-28 md:pb-0">
+        <Hero />
+        <About />
+      </div>
+      <Showroom />
+
+      <CTA />
+      <Footer />
+
+      {/* Background */}
+      <StarsBackground />
+      <ShootingStars />
+    </main>
+  );
 }
