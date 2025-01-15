3f84a2cea047073a100d40b846f8d7969ea1d03c
Letícia Vieira <lvieira268@gmail.com>
2025-01-15 17:38:43 -0300

    feat: creates community-reviews comopnent
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ src/constants/community-reviews.ts
@@ -0,0 +1,58 @@
+import { Review } from "@/sections/testimonials";
+
+export const REVIEWS: Review[] = [
+  {
+    name: "Renan Dantas",
+    username: "@renandantas7357",
+    body: "Aulas de edição! 🔥",
+    img: "https://yt3.ggpht.com/wz894gsRjkUn-T1XKQRrKG-33V6uhb8kjICUcAqeSpYe9TW0-AkHzVNixBN8419CJmfSlj70=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    name: "Felipe Aires",
+    username: "@filipe_aires",
+    body: "Só digo uma coisa, seu editor ou empresa são absurdos. Que intro absurda. Vídeo irado. Pode pagar mais porque tá valendo",
+    img: "https://yt3.ggpht.com/ytc/AIdro_mP6u-a4YHaeLDul-REg5-sXNbLvTiq6ZKAXSCVVXn4lw=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    name: "Royal William Clash",
+    username: "@ROYALWILLIAMCLASH",
+    body: "Isso não é só um video, mas sim um filme💚💚",
+    img: "https://yt3.ggpht.com/ytc/AIdro_lFFW-nj60JfhTG590s75U0dWxzJiGsU07Mq2tqA7-t2yj2XpI=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    name: "Sahel picsart",
+    username: "@picsart.genius",
+    body: "We need a video editing tutorial 😍",
+    img: "assets/peoples/sahel.jpg",
+  },
+  {
+    name: "Vaz",
+    username: "@vaazw_",
+    body: "Caraca, que conteúdo incrível! Nunca um vídeo tinha me envolvido tanto quanto esse... Edição completamente dinâmica e que prende atenção!",
+    img: "assets/peoples/vaaz.jpg",
+  },
+  {
+    name: "MEET FILMMAKING COMMUNITY ®",
+    username: "@filmmkrs",
+    body: "As always , stunning visuals 😮",
+    img: "assets/peoples/filmmkrs.jpg",
+  },
+  {
+    name: "Lucas Pantoja",
+    username: "@pantoja.edit",
+    body: "Meu deus. Realmente não há palavras para o nível desta produção. Os meus mais sinceros parabéns por toda a criatividade envolvida neste projeto.",
+    img: "assets/peoples/pantoja.jpg",
+  },
+  {
+    name: "Nathan Mendes",
+    username: "@na7han_",
+    body: "Tudo no video é perfeito! Do roteiro, a thumbnail, da thumb ao video, e do inicio do video ao final. PARABÉNS!!!",
+    img: "https://yt3.ggpht.com/UIIMk1ffof-Z3Aj18RFX2fPn5r0vswiUU0z1ohK43MMiTwyhs1lMKDHu3R0-6HQg7scOwE3uK2Y=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    name: "Nacaradi",
+    username: "@nacaradi1",
+    body: "Esse vídeo é simplesmente uma obra de arte!",
+    img: "https://yt3.ggpht.com/9wzzYrLzBzwYWmhIwYrHk0kx7ixBhn88JeQerAf4r9QhwBT0obZz6Tekl8oJmTeAqWjQlUJXwg=s88-c-k-c0x00ffffff-no-rj",
+  },
+];
