2ab1163bda8994e1eed208481a66a81a346cca57
Letícia Vieira <lvieira268@gmail.com>
2025-01-15 17:36:54 -0300

    feat: imports new components
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,3 +4,7 @@ export * from "./shooting-stars";
 export * from "./stars-background";
 export * from "./shimmer-button";
 export * from "./animated-text";
+export * from "./map";
+export * from "./keyboard";
+export * from "./logo-cluster";
+export * from "./bento-card";
