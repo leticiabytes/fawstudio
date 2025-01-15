40e1e4dbc74b7fa63cdab1a6acb9bc5524ce3cc6
Letícia Vieira <lvieira268@gmail.com>
2025-01-15 17:38:53 -0300

    feat: creates a new constant
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ src/constants/peoples.ts
@@ -0,0 +1,44 @@
+export const PEOPLES = [
+  {
+    id: 1,
+    name: "Gabriel Saab",
+    designation: "Diretor de Produção",
+    image:
+      "https://static.wixstatic.com/media/215c0f_409978e40c654957aa93f744d6a45612~mv2.png/v1/fill/w_574,h_646,al_c,q_95,enc_avif,quality_auto/GABRIEL-SAAB.png",
+  },
+  {
+    id: 2,
+    name: "João Russo",
+    designation: "Diretor de Marketing",
+    image:
+      "https://static.wixstatic.com/media/215c0f_7c0752e766174812a8de23605653778e~mv2.jpg/v1/crop/x_0,y_392,w_2160,h_2432/fill/w_574,h_646,al_c,q_90,enc_avif,quality_auto/01.jpg",
+  },
+  {
+    id: 3,
+    name: "Arthur Borges",
+    designation: "Diretor de Operações",
+    image:
+      "https://static.wixstatic.com/media/215c0f_5b6992583b384afcbb5d44a094f00410~mv2.jpeg/v1/fill/w_574,h_646,al_c,q_90,enc_avif,quality_auto/WhatsApp%20Image%202024-08-26%20at%2014_44_20.jpeg",
+  },
+  {
+    id: 4,
+    name: "Pitagoras de Samos",
+    designation: "Filósofo e Matermático",
+    image:
+      "https://static.todamateria.com.br/upload/pi/ta/pitagoras-og.jpg?class=ogImageSquare",
+  },
+  {
+    id: 5,
+    name: "Christopher Nolan",
+    designation: "Diretor de Cinema e Roteirista Britânico",
+    image:
+      "https://cdn.britannica.com/37/255737-050-9BB3FEDA/Christopher-Nolan-Movie-film-director-Oppenheimer-UK-premiere-2023.jpg?w=400&h=300&c=crop",
+  },
+  {
+    id: 6,
+    name: "Adele Laurie Blue",
+    designation: "Cantora-compositora e Multi-instrumentista",
+    image:
+      "https://conteudo.imguol.com.br/c/entretenimento/74/2021/10/08/adele-foi-um-sucesso-ou-um-delirio-coletivo-1633701616317_v2_900x506.jpg",
+  },
+];
